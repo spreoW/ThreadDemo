7b65b8e6ba32554bd08367c2feaa47c5f5c70d5d
wangquan <Wq281976715>
2021-05-09 20:46:34 +0800

    dev push

--- src/main/java/com/testThread/Test.java
+++ src/main/java/com/testThread/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hotfix commit");
         System.out.println("dev commit");
+        System.out.println("dev commit and push!");
     }
 }
